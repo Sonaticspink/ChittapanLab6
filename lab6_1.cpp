2464da132559c5affc0876a7391e939ee98802df
Guitar <keetakeeta59@gmail.com>
2023-12-01 13:48:09 +0700

    Lab6_1_filled

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
+    int num,n=0,m=0;
+    while (true){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    if(num%2 == 0 && num != 0){
+        n++;
+    }
+    else{
+        if(num != 0){
+        m++;
+        }
+    }
+    if(num == 0){
+        break;
+    }
+    }
+
+    cout << "#Even numbers = "<<n<<endl;
+    cout << "#Odd numbers = "<<m;
     return 0;
 }
